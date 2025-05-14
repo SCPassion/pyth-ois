934029683153e03067bf89e93aece924fe145472
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-05-14 20:03:20 +0200

    feat: implement routing and add Dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router"
+import Dashboard from "./components/pages/Dashboard"
 function App() {
-  return <>hi</>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/:address" element={<div>Wallet Info</div>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ src/components/pages/Dashboard.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center bg-gray-100">
+      <h1 className="mb-4 text-4xl font-bold">Dashboard</h1>
+      <p className="text-lg">Welcome to the Dashboard!</p>
+    </div>
+  )
+}
